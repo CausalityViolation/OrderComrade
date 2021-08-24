1167ca1307b80d9c05bfd7eeb1f8dcbbe989b3b8
Causality <m.uddner@gmail.com>
2021-08-24 12:11:43 +0200

    update OrderController

--- src/main/java/comradepizza/ordercomrade/controllers/OrderController.java
+++ src/main/java/comradepizza/ordercomrade/controllers/OrderController.java
@@ -2,7 +2,9 @@ package comradepizza.ordercomrade.controllers;
 
 import comradepizza.ordercomrade.entities.PizzaOrder;
 import comradepizza.ordercomrade.repos.OrderRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.io.BufferedReader;
@@ -21,21 +23,54 @@ public class OrderController {
         this.repo = repo;
     }
 
+    public StringBuffer getMenu() throws IOException {
+
+        URL url = new URL("http://localhost:9000/comrade");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        return content;
+    }
+
     @PostMapping("/order/{orderDetails}")
     public String placeOrder(@PathVariable String orderDetails) throws IOException {
 
         StringBuffer content = getMenu();
 
         String availablePizzas = String.valueOf(content);
-        String[] splitOrder = orderDetails.split("[#]");
-        String message = splitOrder[1];
+        String[] splitOrder;
+        String message;
+        if (orderDetails.contains("&")) {
+            splitOrder = orderDetails.split("[&]");
+            message = splitOrder[1];
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE);
+        }
         String[] pizzasOrderedArray = splitOrder[0].split("[$]");
         String pizzasOrderedString = "";
 
+        int pizzaFound = 0;
         for (int i = 0; i < pizzasOrderedArray.length; i++) {
-            if (availablePizzas.contains("\"name\":\"" + pizzasOrderedArray[i] + "\"")) {
-                pizzasOrderedString += pizzasOrderedArray[i] + "$" + pizzasOrderedArray[i + 1] + "$";
+            try {
+                int amount = Integer.parseInt(pizzasOrderedArray[i]);
+                pizzasOrderedString += amount;
+            } catch (NumberFormatException e) {
+                if (availablePizzas.contains("\"name\":\"" + pizzasOrderedArray[i] + "\"")) {
+                    pizzasOrderedString += pizzasOrderedArray[i];
+                    pizzaFound++;
+                }
             }
+        }
+        if (pizzaFound == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         PizzaOrder pizzaOrder = new PizzaOrder(message, pizzasOrderedString);
@@ -81,5 +116,11 @@ public class OrderController {
         return getMenu();
     }
     //order/PIZZA$ANTAL$PIZZA$ANTAL#MEDDELANDE
+
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
 
 }
